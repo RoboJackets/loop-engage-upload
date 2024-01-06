5f6c870ba420976a6b45ce3f7d66546ebcaa077c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-01-06 17:22:49 -0500

    Fix import order

--- loop_engage_upload.py
+++ loop_engage_upload.py
@@ -12,9 +12,9 @@ from requests import get, post, put
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.webdriver import WebDriver
 
 from webdriver_manager.chrome import ChromeDriverManager
 
